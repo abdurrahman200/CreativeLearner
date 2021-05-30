4fb2fde134774a655e533ba0fab3b35074df916e
Abdur rahman <abdur.rahman200301@gmail.com>
2021-05-30 17:36:33 +0600

    Redux addded

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -3,7 +3,7 @@ import { Button, Menu } from "antd";
 import { Header } from "antd/lib/layout/layout";
 import { Link } from "react-router-dom";
 import { useAuth } from "../Authentication/Auth/useAuth";
-import { ShoppingCartOutlined, LogoutOutlined } from "@ant-design/icons";
+import { LogoutOutlined } from "@ant-design/icons";
 
 const Navbar = () => {
   const auth = useAuth();
@@ -15,26 +15,27 @@ const Navbar = () => {
   };
   return (
     <Header>
-      <Link className="logo" to="/" style={{ fontSize: "2rem", fontWeight: "500" }}>
+      <Link
+        className="logo"
+        to="/"
+        style={{ fontSize: "2rem", fontWeight: "500" }}
+      >
         <span style={{ color: "#ff4d4f" }}> Creative </span> Learner
       </Link>
       <Menu theme="dark" mode="horizontal" style={{ float: "right" }}>
-        <Button type="primary" ghost style={{ margin: "0 1rem" }}>
-          <Link to="/About"> About </Link>
-        </Button>
-        <Button type="primary" ghost >
-          <Link to="/Success"> Success </Link>
-        </Button>
         {auth.user ? (
           <Button type="primary" ghost style={{ margin: "0 1rem" }}>
             <Link to="/Course"> My Course</Link>
           </Button>
         ) : (
-          <Button type="primary" ghost style={{ margin: "0 1rem" }}>
-            <Link to="/Payments">
-              <ShoppingCartOutlined /> Enroll
-            </Link>
-          </Button>
+          <>
+            <Button type="primary" ghost style={{ margin: "0 1rem" }}>
+              <Link to="/About"> About </Link>
+            </Button>
+            <Button type="primary" ghost>
+              <Link to="/Success"> Success </Link>
+            </Button>
+          </>
         )}
 
         {auth.user ? (

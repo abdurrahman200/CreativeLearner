751778786b96072fde13db18e28acccdf8c540ef
Aminul Islam <aminulislam565688@gmail.com>
2021-05-30 22:02:06 +0600

    i added payment system

--- src/Components/Payments/PaymentGateway.jsx
+++ src/Components/Payments/PaymentGateway.jsx
@@ -1,5 +1,6 @@
-import React from "react";
 import { Card, Col, Row } from "antd";
+import React from "react";
+import PaymentForm from './PaymentsForm';
 
 const PaymentGateway = () => {
   return (
@@ -10,7 +11,7 @@ const PaymentGateway = () => {
         style={{ padding: "3rem 0", textAlign: "center" }}
       >
         <Col xs={23} sm={12} md={12} lg={6} xl={6} xxl={6}>
-          <Card title=" Payments Option">{/* White Code  */}</Card>
+          <Card title=" Payments Option">{<PaymentForm/>}</Card>
         </Col>
       </Row>
     </div>
